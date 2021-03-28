670337a850aa064bda74b2bd46b7c39bd9658b93
Aditya Ganti <adi10rox@gmail.com>
2021-03-28 06:44:31 +0530

    Update getScore.js
    combined into a single function

--- src/utils/getScore.js
+++ src/utils/getScore.js
@@ -1,16 +1,12 @@
-function getScore(accuracy) {
+function getScore(reqdAngle, obtdAngle) {
+    
+    var deviation = Math.abs(obtdAngle - reqdAngle);
+    var accuracy = 100 - ((100/180)*deviation);
+
     if(accuracy<70||accuracy>100)
         return 0;
 
     else {
         return accuracy;
     }
-}
-
-function getAccuracy(reqdAngle, obtdAngle) {
-    
-    var deviation = Math.abs(obtdAngle - reqdAngle);
-    var accuracy = 100 - ((100/180)*deviation);
-
-    return getScore(accuracy);
 }
